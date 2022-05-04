4fdec482c16e34bceea0e0450271ef249ea33da5
CODE <ahmedali19420@gmail.com>
2022-05-04 14:55:55 +0200

    V2.0.9

--- app/Http/Controllers/API/Admins/AdminController.php
+++ app/Http/Controllers/API/Admins/AdminController.php
@@ -56,7 +56,7 @@ class AdminController extends Controller
 
     public function updateUser($id , Request $request)
     {
-
+        // get user from DB
         $user = User::find($id);
         if (!is_null($user))
         {
